40d4980f65f671f0dd567bbe75830e982ddfc286
shiv-shrihari5 <patelshiv512@gmail.com>
2025-07-13 02:07:15 -0700

    fine

---
+++ task1.py
@@ -0,0 +1,6 @@
+num=int(input("enter the number"))
+total = 1
+for i in range(1, num+1):
+    total *= i
+
+print("The sum is:", total)
---
+++ task2.py
@@ -0,0 +1,14 @@
+import math
+A =int(input("Enter a number"))
+#To find sqare root
+i=A/2
+print("thr Sqare root is: ",i)
+#To find nachrael log
+number = A
+natural_log = math.log(number)
+print(f"The  Natural logarithm of {number} is" ,natural_log)
+# to find sin value
+print("The value of sin is : ", math.sin(A))
+
+
+      
